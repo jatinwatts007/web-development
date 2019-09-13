b06fffebc7a3212000731225778b09debdc1f5f0
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2019-09-13 23:24:33 +0530

    Create pattern object.cpp

---
+++ pattern object.cpp
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="https://kit.fontawesome.com/944171a9d8.js"></script>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Pattern</title>
+    <style type="text/css">
+      #header{
+        background: url("https://www.vandersandengroup.co.uk/sites/default/files/styles/front_style_1/public/homepage/vds-paneel-zv-123_rega.jpg?itok=7_Y_7Qfh") center center / cover no-repeat ;
+      }
+      h1{
+        font-color: white;
+      }
+    </style>
+  </head>
+  <body>
+    <nav class="navbar bg-dark navbar-dark">
+      <div class="container">
+      <a href=""class="navbar-brand"><i class="fas fa-binoculars mr-2"></i>Pattern</a>
+      </div>
+    </nav>
+    <section id="header" class="jumbotron text-center">
+        <h1 class="display-3">PATTERN</h1>
+        <p class="lead">lorem ipsum dolor sit, amet consectetur adipsicing elt,maicres,pariatur?
+        </p>
+        <a href="" class="btn btn-primary">do something</a>
+        <a href="" class="btn btn-success">another thing</a>
+
+    </section>
+    <section id="gallery">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 mb-4">
+        <div class="card">
+          <img class="card-img-top"src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7I46jOBhvkSwUNoqKV7PQymlN5cP6XmnZrKje_eIN_CEDv2SRIw">
+          <div class="card-body">
+            <h5 class="card-title">first thing</h5>
+            <p class="card-body">This is photo 1.</p>
+            <a href="" class="btn btn-outline-success btn-sm">Download</a>
+            <a href=""class="btn btn-outline-danger btn-sm"><i class="far fa-heart"></i></a>
+            </div>
+          </div>
+        </div>
+         <div class="col-lg-4 mb-4">
+        <div class="card">
+          <img class="card-img-top"src="https://i2.wp.com/explorista.net/wp-content/uploads/2019/02/beautiful-places-in-the-netherlands.jpg?fit=1270%2C846&ssl=1">
+          <div class="card-body">
+            <h5 class="card-title">first thing</h5>
+            <p class="card-body">This is photo 1.</p>
+            <a href="" class="btn btn-outline-success btn-sm">Download</a>
+            <a href=""class="btn btn-outline-danger btn-sm"><i class="far fa-heart"></i></a>
+            </div>
+          </div>
+        </div>
+         <div class="col-lg-4 mb-4">
+        <div class="card">
+          <img class="card-img-top"src="https://amp.businessinsider.com/images/526ebac569bedd56578b457b-750-562.jpg">
+          <div class="card-body">
+            <h5 class="card-title">first thing</h5>
+            <p class="card-body">This is photo 1.</p>
+            <a href="" class="btn btn-outline-success btn-sm">Download</a>
+            <a href=""class="btn btn-outline-danger btn-sm"><i class="far fa-heart"></i></a>
+          </div>
+        </div>
+        </div>
+      </div>
+      </div>
+    </section>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
