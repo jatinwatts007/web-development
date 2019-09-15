554012186a47e34aa6aa38a7529e62910e2c160e
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2019-09-15 23:06:23 +0530

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,10 @@
+/*var answer=prompt("are we there yet");
+while(answer !== "yes" && answer!=="yeah"){
+var answer=prompt("are we there yet");
+}
+alert("yes you made it");*/
+var answer=prompt("are you there yet");
+while(answer.indexOf("yes") === -1){
+var answer=prompt("are you there yet");	
+}
+alert("yes you made it");
