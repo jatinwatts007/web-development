2eb3a35f6e30df3417d0cdf0acf1033b64312611
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2019-09-20 23:08:26 +0530

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,6 @@
+var  user=prompt("whats your first name!");
+var name=prompt("whats you last name");
+var age=prompt("how old are u");
+
+console.log("great to see you "+user+" " +name );
+console.log("you are "+age +" years old" );
